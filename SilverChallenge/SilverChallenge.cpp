af88de4ecfe70460df562316aad81245de7c13c3
Adam <gillesad@gmail.com>
2026-03-11 10:48:53 +0000

    Moved variables into tasks rather than having them global

--- SilverChallenge/SilverChallenge.cpp
+++ SilverChallenge/SilverChallenge.cpp
@@ -1,34 +1,19 @@
 #include "x_14_buggy.h"
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h> 
-constexpr uint8_t N = 200;
 
-// Networking objects
-Buffer<N> inBuffer, outBuffer;
-WiFiServer server(wifi::PORT);
-WiFiClient GUI;
-
-// Hardware objects
-ROB12629 leftEncoder(2), rightEncoder(3);
-HCSR04 ears(5, 4);
-L293D driver(6,7,11,12,9,10);
-
-// Declare Interrupt Service Routines
+// Declaration of Interrupt Service Routines
 void leftISR(); void rightISR();
 
 // Tasks (for scheduler to handle)
-void read(void *);
+void telemetry(void *);
 void sense(void *);
-void send(void *);
 
 // Declare handles for each task, used to pass to task-analysing functions like uxTaskGetStackHighWaterMark()
-TaskHandle_t readHandle, senseHandle, sendHandle;
+TaskHandle_t telemetryHandle, senseHandle;
 
 // Set up semaphores (I think we just need a mutex?)
-SemaphoreHandle_t GUIMutex;
-
-// NOTE: We'll need to sort out the GUI.print() in random sections of code. We can use a mutex and lock/unlock but I think we'd
-// prefer using an outBuffer to write into locally, then set up a mutex around that local buffer (local to the arduino, that is)
+SemaphoreHandle_t statMutex;
 
 void setup() {
   // Set up serial for debugging
@@ -36,19 +21,16 @@ void setup() {
 
   // Set up network
   wifi::initialiseAccessPoint();
-  server.begin();
 
-  // Set up hardware
-  driver.begin();
-  ears.begin();
-  // We pass ISRs to the encoders
-  leftEncoder.begin(leftISR);
-  rightEncoder.begin(rightISR);
+
 
   // Set up tasks
-  xTaskCreate(read, "Read from GUI", 1024, nullptr, 2, readHandle);
-  xTaskCreate(sense, "Sense and drive", 1024, nullptr, 3, senseHandle);
-  xTaskCreate(send, "Send back to GUI", 1024, nullptr, 1, sendHandle);
+  xTaskCreate(telemetry, "Read from GUI", 1024, nullptr, 1, telemetryHandle);
+  xTaskCreate(sense, "Sense and drive", 1024, nullptr, 2, senseHandle);
+
+
+  // Start scheduler (does not return)
+  vTaskStartScheduler();
 }
 
 void loop() {
@@ -65,7 +47,14 @@ void rightISR(){
 }
 
 // - TASK 1 -
-void read(void *parameters){
+void telemetry(void *parameters){
+  // Networking objects
+  Buffer<200> inBuffer, outBuffer;
+  WiFiServer server(wifi::PORT);
+  server.begin();
+
+  WiFiClient GUI;
+
   while(true){
     // Reconnect if disconnected
     keep(GUI, server);
@@ -74,36 +63,36 @@ void read(void *parameters){
     // Handle a command if there is one
     handle(inBuffer, driver);
 
-    // Don't repeat this task again for at least 20 ms
-    vTaskDelay(pdMS_TO_TICKS(20));
+    float distance;
+
+    // We've only got one thing to send right now
+    sendDistance(GUI, distance);
+    printStackUsage();
+
+    clock = (clock + 1) % numberOfSends;
+
+    // Don't repeat this task again for at least 300 ms
+      vTaskDelay(pdMS_TO_TICKS(200));
   }
 }
 
 // - TASK 2 -
 void sense(void *parameters){
+  // Hardware objects
+  ROB12629 leftEncoder(2), rightEncoder(3); // THIS NEEDS TO BE CHANGED
+  HCSR04 ears(5, 4); // PINS NEED TO BE CHANGED
+  L293D driver(6,7,11,12,9,10);
 
-}
+  // Set up hardware
+  driver.begin();
+  ears.begin();
+  // We pass ISRs to the encoders
+  leftEncoder.begin(leftISR);
+  rightEncoder.begin(rightISR);
 
-// - TASK 3 -
-void send(void *parameters){
   while(true){
-      float distance;
 
-      if (xSemaphoreTake(sensorMutex, pdMS_TO_TICKS(5)) == pdTRUE) {
-          distance = sensors.distance;
-          leftIR = sensors.leftIR;
-          rightIR = sensors.rightIR;
-          xSemaphoreGive(sensorMutex);
-      }
 
-      // We've only got one thing to send right now
-      sendDistance(GUI, distance);
-      printStackUsage();
-
-      clock = (clock + 1) % numberOfSends;
-
-    // Don't repeat this task again for at least 300 ms
-      vTaskDelay(pdMS_TO_TICKS(300));
   }
 }
 
@@ -113,20 +102,16 @@ void printStackUsage() {
   // NOTE (2): a high water mark is the highest point of stack usage so far (or something like that)
   // NOTE (3): a word is the fundamental unit of memory the MCU has (computers are generally layed out in 32 or 64 bit chunks)
 
-    UBaseType_t readHW = uxTaskGetStackHighWaterMark(readHandle);
-    sendEvent(GUI, "Read from GUI high water mark (words): ", readHW);
-    // Serial.print("Read from GUI high water mark (words): ");
-    // Serial.println(readHW);
+    UBaseType_t telemetryHW = uxTaskGetStackHighWaterMark(telemetryHandle);
+    sendEvent(GUI, "Networking high water mark (words): ", telemetryHW);
+    // Serial.print("Networking high water mark (words): ");
+    // Serial.println(telemetryHW);
 
     UBaseType_t senseHW = uxTaskGetStackHighWaterMark(senseHandle);
     sendEvent(GUI, "Sensor and drive high water mark (words): ", senseHW);
     // G.print("Sensor and drive high water mark (words): ");
     // Serial.println(senseHW);
 
-    UBaseType_t sendHW = uxTaskGetStackHighWaterMark(sendHandle);
-    sendEvent(GUI, "Sending to GUI high water mark (words): ", sendHW);
-    // Serial.print("Sending to GUI high water mark (words): ");
-    // Serial.println(sendHW);
 }
 
 
